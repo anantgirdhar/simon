96a046dbafe63b8a4e3a42794a3d53155474d80e
Anant Girdhar <anant.girdhar@proton.me>
2024-02-14 18:52:21 -0500

    Add command line arguments
    This commit refactors the main code and allows passing in a few command
    line arguments to modify the functionality without having to hard code
    them in the script. Some of this functionality might be, perhaps, better
    expressed as a separate object, but for now this seems to work just
    fine.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 import subprocess
+import sys
 import time
 from decimal import Decimal
 from pathlib import Path
+from typing import Callable
 
 from taskqueue import TaskQueue
 from tasks import (DeleteReconstructedTask, DeleteSplitTask, ReconstructTask,
@@ -11,19 +13,28 @@ from tasks import (DeleteReconstructedTask, DeleteSplitTask, ReconstructTask,
                    timestamp_successfully_reconstructed)
 
 
-def is_to_be_deleted(t: str) -> bool:
-    if t == "0":
-        return True
-    # This timestamp is to be deleted if it is not a "multiple" of 1e-5
-    # First convert it to a Decimal so that we don't have to deal with floating
-    # point weirdnesses
-    timestamp = Decimal(t)
-    # The timestamp is a "multiple" of 1e-5 if the timestamp times 1e5 does not
-    # have a fractional part
-    if (timestamp * int(1e5)) % 1 > 0:
-        return True
-    else:
-        return False
+def create_deletion_condition_function(
+    keep_every: str,
+) -> Callable[[str], bool]:
+    def is_to_be_deleted(t: str) -> bool:
+        if t == "0":
+            return True
+        # This timestamp is to be deleted if it is not a "multiple" of
+        # keep_every
+        # First convert it to a Decimal so that we don't have to deal with
+        # floating point weirdnesses
+        timestamp = Decimal(t)
+        # The timestamp is a "multiple" of keep_every if the timestamp times
+        # (1/keep_every) does not have a fractional part
+        # But we also have to make sure to account for floating point
+        # weirdnesses with (1/keep_every)
+        factor = Decimal(int(round(1 / float(keep_every), 1)))
+        if (timestamp * factor) % 1 > 0:
+            return True
+        else:
+            return False
+
+    return is_to_be_deleted
 
 
 def initiate_incomplete_tasks_from_last_run(task_queue: TaskQueue) -> None:
@@ -70,32 +81,75 @@ def in_openfoam_root_case_dir() -> bool:
     return True
 
 
-def main() -> None:
-    if not in_openfoam_root_case_dir():
-        raise Exception("This is not an OpenFOAM root case dir.")
-    task_queue = TaskQueue()
-    initiate_incomplete_tasks_from_last_run(task_queue)
-    times = sorted(
+def find_and_add_tasks(
+    task_queue: TaskQueue,
+    processed_times: list[str],
+    is_to_be_deleted: Callable[[str], bool],
+) -> list[str]:
+    times_found = sorted(
         [d.name for d in Path("processor0").glob("[0-9]*") if d.is_dir()],
         key=float,
     )
-    for i, t in enumerate(times):
-        print(f"\nIn processing loop, processing {t}")
-        print(task_queue)
-        if i == len(times) - 1:
-            # If it is the last time in the list, don't do anything else
+    for i, t in enumerate(times_found):
+        if t in processed_times:
             continue
+        print(f"\nIn find-and-add loop, processing {t}")
+        print(task_queue)
+        if i == len(times_found) - 1:
+            # If it is the last time in the list, don't do anything else
+            break
         if is_to_be_deleted(t):
             # If this time is to be deleted, then delete this
             task_queue.add(DeleteSplitTask(t))
         else:
             task_queue.add(ReconstructTask(t, post_cleanup=True))
-    while task_queue:
-        print("\nIn update loop")
-        print(task_queue)
-        task_queue.update()
-        time.sleep(3)
+        processed_times.append(t)
+    return processed_times
+
+
+def main(
+    keep_every: str,
+    sleep_time_per_update: int = 2,
+    recheck_every_num_updates: int = 1,
+) -> None:
+    num_updates = 0
+    if not in_openfoam_root_case_dir():
+        raise Exception("This is not an OpenFOAM root case dir.")
+    task_queue = TaskQueue()
+    initiate_incomplete_tasks_from_last_run(task_queue)
+    processed_times: list[str] = []
+    is_to_be_deleted = create_deletion_condition_function(keep_every)
+    processed_times = find_and_add_tasks(
+        task_queue, processed_times, is_to_be_deleted
+    )
+    while len(task_queue) > 0 or recheck_every_num_updates > 0:
+        num_updates += 1
+        if len(task_queue) > 0:
+            print("\nUpdating task queue")
+            print(task_queue)
+            task_queue.update()
+        time.sleep(sleep_time_per_update)
+        if recheck_every_num_updates <= 0 and len(task_queue) == 0:
+            return
+        if (
+            recheck_every_num_updates > 0
+            and num_updates % recheck_every_num_updates == 0
+        ):
+            processed_times = find_and_add_tasks(
+                task_queue, processed_times, is_to_be_deleted
+            )
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 3:
+        print(
+            f"Usage: {sys.argv[0]}"
+            " keep_every"
+            " sleep_time_per_update"
+            " [recheck_every_num_updates]"
+        )
+        sys.exit(-1)
+    keep_every = sys.argv[1]
+    sleep_time_per_update = int(sys.argv[2])
+    recheck_every_num_updates = int(sys.argv[3]) if len(sys.argv) > 3 else 1
+    main(keep_every, sleep_time_per_update, recheck_every_num_updates)
