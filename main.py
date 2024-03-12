a51b832acaf05d8c989914ad33338ac292f33eef
Anant Girdhar <anant.girdhar@proton.me>
2024-03-12 12:44:12 -0400

    Create command line interface for simon
    This commit introduces a command line interface for simon that allows
    the two different modes (setup and monitor) to be started.

--- main.py
+++ main.py
@@ -1,30 +1,94 @@
 #!/usr/bin/python3
 
-import sys
+import argparse
 import time
+from decimal import Decimal
 from pathlib import Path
 
 from simon.oflistener import OFListener
 from simon.taskqueue import TaskQueue
 
 
-def main(
-    keep_every: str,
-    num_simultaneous_tasks: int = 6,
-    sleep_time_per_update: int = 2,
-    recheck_every_num_updates: int = 1,
+def init_argparse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog="simon", description="A Simulation Monitor"
+    )
+    parser.add_argument(
+        "command",
+        choices=["setup", "monitor"],
+        help="Running mode 'setup' (clean up case directory) or 'monitor'",
+    )
+    parser.add_argument(
+        "--keep-every",
+        required=True,
+        dest="keep_every",
+        type=Decimal,
+        help="How often to keep timesteps (all others are deleted)",
+    )
+    parser.add_argument(
+        "-n",
+        "--num-simultaneous-tasks",
+        default=4,
+        dest="num_simultaneous_tasks",
+        type=int,
+        help="How many tasks to run in parallel",
+    )
+    parser.add_argument(
+        "-s",
+        "--sleep-time-per-update",
+        default=2,
+        dest="sleep_time_per_update",
+        type=int,
+        help="How many seconds to sleep for between update steps",
+    )
+    parser.add_argument(
+        "-u",
+        "--recheck-every-num-updates",
+        default=1,
+        dest="recheck_every_num_updates",
+        type=int,
+        help="How many update steps to run before querying for new tasks",
+    )
+    return parser
+
+
+def setup(
+    keep_every: Decimal,
+    num_simultaneous_tasks: int,
+    sleep_time_per_update: int,
+    case_directory: Path = Path("."),
+) -> None:
+    listener = OFListener(keep_every, case_directory)
+    task_queue = TaskQueue(num_simultaneous_tasks=num_simultaneous_tasks)
+    task_queue.add(*listener.get_cleanup_tasks())
+    # Run all the tasks in the task queue to completion before proceeding
+    while True:
+        print("\nUpdating task queue")
+        task_queue.update()
+        print(task_queue)
+        if len(task_queue) == 0:
+            break
+        time.sleep(sleep_time_per_update)
+    listener.ensure_case_correctness()
+
+
+def monitor(
+    keep_every: Decimal,
+    num_simultaneous_tasks: int,
+    sleep_time_per_update: int,
+    recheck_every_num_updates: int,
+    case_directory: Path = Path("."),
 ) -> None:
     num_updates = 0
-    listener = OFListener(keep_every, Path("."))
+    listener = OFListener(keep_every, case_directory)
     task_queue = TaskQueue(num_simultaneous_tasks=num_simultaneous_tasks)
-    # initiate_incomplete_tasks_from_last_run(task_queue)
     task_queue.add(*listener.get_new_tasks())
     while len(task_queue) > 0 or recheck_every_num_updates > 0:
         num_updates += 1
         if len(task_queue) > 0:
             print("\nUpdating task queue")
-            print(task_queue)
             task_queue.update()
+            print(task_queue)
         time.sleep(sleep_time_per_update)
         # if recheck_every_num_updates <= 0 and len(task_queue) == 0:
         #     return
@@ -35,23 +99,23 @@ def main(
             task_queue.add(*listener.get_new_tasks())
 
 
-if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print(
-            f"Usage: {sys.argv[0]}"
-            " keep_every"
-            " num_simultaneous_tasks"
-            " sleep_time_per_update"
-            " [recheck_every_num_updates]"
+def main() -> None:
+    parser = init_argparse()
+    args = parser.parse_args()
+    if args.command == "setup":
+        setup(
+            args.keep_every,
+            args.num_simultaneous_tasks,
+            args.sleep_time_per_update,
         )
-        sys.exit(-1)
-    keep_every = sys.argv[1]
-    num_simultaneous_tasks = int(sys.argv[2])
-    sleep_time_per_update = int(sys.argv[3])
-    recheck_every_num_updates = int(sys.argv[4]) if len(sys.argv) > 4 else 1
-    main(
-        keep_every,
-        num_simultaneous_tasks,
-        sleep_time_per_update,
-        recheck_every_num_updates,
-    )
+    elif args.command == "monitor":
+        monitor(
+            args.keep_every,
+            args.recheck_every_num_updates,
+            args.num_simultaneous_tasks,
+            args.sleep_time_per_update,
+        )
+
+
+if __name__ == "__main__":
+    main()
