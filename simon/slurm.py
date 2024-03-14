8ea5519b78d8b806294ba733cac7fb295b90ab3a
Anant Girdhar <anant.girdhar@proton.me>
2024-03-13 22:29:02 -0400

    Add slurm job manager

---
+++ simon/slurm.py
@@ -0,0 +1,202 @@
+import os
+import re
+import shutil
+import subprocess
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Iterator, List
+
+from simon.task import Task
+
+JOB_NAME_REGEX = re.compile(r"#SBATCH\s+-J\s+([a-zA-Z0-9_-]+)$")
+
+
+class SlurmJobManager:
+    def __init__(
+        self, case_dir: Path, job_sfile: str, job_id: str, compress_sfile: str
+    ) -> None:
+        self.case_dir = case_dir
+        self.job_sfile = job_sfile
+        self.job_id = job_id
+        self.compress_sfile = compress_sfile
+        self._verify_directory_is_valid()
+        self.job_name = self._read_job_name()
+
+    def _verify_directory_is_valid(self) -> None:
+        if not (self.case_dir / self.job_sfile).is_file():
+            raise ValueError(
+                f"{self.case_dir} does not contain {self.job_sfile}"
+            )
+        # TODO: Ensure that compress_sfile is present
+        # TODO: Ensure that the job ID is correct
+
+    def _read_job_name(self) -> str:
+        with open(self.case_dir / self.job_sfile) as f:
+            # TODO: Is this correct??
+            for line in f:
+                if m := re.search(JOB_NAME_REGEX, line):
+                    return m.group(1)
+        raise Exception("Unable to find job name.")
+
+    def get_new_tasks(self) -> List[Task]:
+        raise NotImplementedError
+        new_tasks: List[Task] = []
+        # Get the current state of the files
+        return new_tasks
+
+    def get_job_status(self, job_id: str) -> str:
+        squeue_output = (
+            subprocess.check_output(
+                f"squeue --me --jobs={job_id} -o '%i %j %T'", shell=True
+            )
+            .decode("utf-8")
+            .split("\n")
+        )
+        # Remove any empty lines
+        while "" in squeue_output:
+            squeue_output.remove("")
+        # If there is only the one header line, then this job does not exist
+        if len(squeue_output) == 1:
+            return "JOB_NOT_FOUND"
+        if len(squeue_output) < 1 or len(squeue_output) > 2:
+            raise ValueError(
+                f"Got something weird back from squeue:\n{squeue_output}"
+            )
+        # Otherwise the status is the third element
+        return squeue_output[1].split(" ")[2]
+
+    def get_job_name(self, job_id: str) -> str:
+        squeue_output = (
+            subprocess.check_output(
+                f"squeue --me --jobs={job_id} -o '%i %j %T'", shell=True
+            )
+            .decode("utf-8")
+            .split("\n")
+        )
+        # Remove any empty lines
+        while "" in squeue_output:
+            squeue_output.remove("")
+        # If there is only the one header line, then this job does not exist
+        if len(squeue_output) == 1:
+            return "JOB_NOT_FOUND"
+        if len(squeue_output) < 1 or len(squeue_output) > 2:
+            raise ValueError(
+                f"Got something weird back from squeue:\n{squeue_output}"
+            )
+        # Otherwise the job name is the second element
+        return squeue_output[1].split(" ")[1]
+
+    def requeue_job(self) -> None:
+        if not (self.case_dir / self.job_sfile).is_file():
+            raise FileNotFoundError(f"sfile {self.job_sfile} does not exist")
+        if (new_job_name := self._read_job_name()) != self.job_name:
+            raise AttributeError(
+                f"The job name in the sfile {new_job_name} does not match"
+                f" what this was started with {self.job_name}"
+            )
+        cwd = os.getcwd()
+        commands = [
+            f"cd {self.case_dir}",
+            f"sbatch --parsable -d afterany:{self.job_id} {self.job_sfile}",
+            f"cd {cwd}",
+        ]
+        # TODO: Make sure that this gets higher priority than anyone else
+        Task(command=" && ".join(commands), priority=0).run(block=True)
+
+    def _create_compress_command(self, tgz_file: str, files: List[str]) -> str:
+        tar_command = f"tar -czvf {tgz_file}.inprogress.$SLURM_JOB_ID"
+        for f in files:
+            tar_command += f" {f}"
+        commands = [
+            f"mv {tgz_file}.queued {tgz_file}.inprogress.$SLURM_JOB_ID",
+            tar_command,
+            f"mv {tgz_file}.inprogress.$SLURM_JOB_ID {tgz_file}",
+            f"echo Done compressing {tgz_file}!",
+        ]
+        return " && ".join(commands)
+
+    @contextmanager
+    def _create_compress_sfile(self, compress_command: str) -> Iterator[Path]:
+        # Create a copy of the compress sfile template
+        filled_compress_sfile = f"{self.compress_sfile}.filled"
+        filled_compress_sfile_path = self.case_dir / filled_compress_sfile
+        shutil.copy(
+            self.case_dir / self.compress_sfile,
+            filled_compress_sfile_path,
+        )
+        # Add the compress_command to it
+        with open(filled_compress_sfile_path, "a") as outfile:
+            outfile.write(f"\n\n{compress_command}")
+        # Yield the name of the file
+        yield filled_compress_sfile_path
+        # Now clean up the file
+        filled_compress_sfile_path.unlink()
+
+    def compress(self, tgz_file: str, files: List[str]) -> None:
+        # Compress some files to out_file then delete the files that were
+        # compressed
+        # This function needs to be aware of the state of this job possibly
+        # across multiple runs, i.e., it needs to somehow be awaare that it the
+        # compression is in progress even if it is restarted
+        # Here is my attempt at keeping track of that:
+        # - The tgz_file name should be unique
+        # - Create a blank file in the output directory as a placeholder for
+        #   the job being queued
+        # - Then create a compression job that replaces this blank file with an
+        #   "inprogress" compressed file that has the SLURM JOB_ID appended to
+        #   it
+        # - Before attempting to start the compression, check if the "queued"
+        #   file or the "inprogress" file exist
+        # - If the "inprogress" file exists, check if the job is still running
+        # - If not, go ahead and submit a new compression job
+        # This method allows to monitor the state of the compression without
+        # having to maintain an extra state file. It also provides some
+        # robustness against changing job names for the main task since the
+        # state of the job is tied to the tgz_file requested (which hopefully
+        # will be unique).
+        # First make sure the inputs are good
+        if not tgz_file:
+            raise ValueError("No output tgz_file specified")
+        if " " in tgz_file:
+            raise ValueError(f"No spaces allowed in file names ({tgz_file})")
+        if not files:
+            raise ValueError("No files to compress")
+        for f in files:
+            if " " in f:
+                raise ValueError(f"No spaces allowed in file names ({f})")
+            if not (self.case_dir / f).is_file():
+                raise FileNotFoundError(f"File {f} not found")
+        if not (self.case_dir / self.compress_sfile).is_file():
+            raise FileNotFoundError(
+                f"sfile {self.compress_sfile} does not exist"
+            )
+        tgz_path = self.case_dir / tgz_file
+        # Next make sure that we haven't already run it
+        queued_file = tgz_path.with_name(tgz_file + ".queued")
+        if queued_file.is_file():
+            return
+        inprogress_glob = self.case_dir.glob(f"{tgz_file}.inprogress.*")
+        for match in inprogress_glob:
+            inprogress_file = match.name
+            inprogress_job_id = inprogress_file.split(".")[-1]
+            status = self.get_job_status(inprogress_job_id)
+            if status != "JOB_NOT_FOUND":
+                # The job exists in slurm
+                return
+        if tgz_path.is_file():
+            # The tar file already exists
+            # Nothing to do here
+            return
+        # inprogress_glob = self.case_dir.glob(f"{tgz_file}.inprogress.*")
+        # # Check if any of the state files exist for this tar
+        # Finally, we can run the compress command
+        with self._create_compress_sfile(
+            self._create_compress_command(tgz_file, files)
+        ) as filled_compress_sfile:
+            commands = [
+                f"touch {tgz_path}.queued",
+                f"sbatch {filled_compress_sfile}",
+            ]
+            Task(command=" && ".join(commands), priority=0).run(block=True)
+        # TODO: As a possible improvement, rename the queued file so that it
+        # contains the job ID
---
+++ tests/test_slurm.py
@@ -0,0 +1,448 @@
+import os
+import shutil
+from datetime import datetime
+from decimal import Decimal
+from pathlib import Path
+from typing import List
+from unittest import mock
+
+import pytest
+from simon.slurm import SlurmJobManager
+from simon.task import Task
+
+JOB_SFILE_NAME = "case.sbatch"
+COMPRESS_SFILE_NAME = "compress.sbatch.template"
+SLURM_JOB_ID = "19810412"
+COMPRESS_JOB_ID = "513849050313"
+
+TEST_VARIABLES = ["U", "T", "p", "H2"]
+
+NUM_PROCESSORS = 16
+TEST_TIMESTAMP_STRINGS = [
+    "0.001",
+    "0.003",
+    "0.005",
+    "0.01",
+    "0.03",
+    "0.05",
+    "0.1",
+    "0.3",
+    "0.5",
+    "1",
+    "3",
+    "5",
+    "10",
+]
+TEST_TIMESTAMP_DECIMALS = list(
+    sorted(set(Decimal(t) for t in TEST_TIMESTAMP_STRINGS))
+)
+
+
+# Case directory setup convenience functions
+
+
+@pytest.fixture
+def case_dir(tmp_path: Path) -> Path:
+    directory = tmp_path
+    sample_job_sfile = Path(__file__).with_name("sample_case.sbatch")
+    sample_compress_sfile = Path(__file__).with_name("sample_compress.sbatch")
+    if not sample_job_sfile.is_file():
+        raise FileNotFoundError(f"{sample_job_sfile} not found")
+    if not sample_compress_sfile.is_file():
+        raise FileNotFoundError(f"{sample_compress_sfile} not found")
+    destination_job_sfile = directory / JOB_SFILE_NAME
+    shutil.copy(sample_job_sfile, destination_job_sfile)
+    destination_compress_sfile = directory / COMPRESS_SFILE_NAME
+    shutil.copy(sample_compress_sfile, destination_compress_sfile)
+    return directory
+
+
+@pytest.fixture
+def cluster(case_dir: Path) -> SlurmJobManager:
+    return SlurmJobManager(
+        case_dir=case_dir,
+        job_sfile=JOB_SFILE_NAME,
+        job_id=SLURM_JOB_ID,
+        compress_sfile=COMPRESS_SFILE_NAME,
+    )
+
+
+@pytest.fixture
+def files_list(case_dir: Path) -> List[str]:
+    # Create the files and return the list of them
+    files = ["1e-4", "0.001", "0.01", "0.1", "1", "10.0", "100"]
+    for f in files:
+        # Write out some information to these files so that they're unique
+        file_contents = f"File name: {f}\nCreated at: {datetime.now()}"
+        with open(case_dir / f, "w") as outfile:
+            outfile.write(file_contents)
+    return files
+
+
+@pytest.fixture
+def directories_list(case_dir: Path) -> List[str]:
+    # Create the directories, with some files and return the list of them
+    directories = ["1e-4", "0.001", "0.01", "0.1", "1", "10.0", "100"]
+    files = ["file1.txt", "file2.txt", "file3.txt"]
+    for d in directories:
+        # Create the directory
+        (case_dir / d).mkdir()
+        for f in files:
+            # Write out some information to these files so that they're unique
+            file_contents = f"File name: {d}/{f}\nCreated at: {datetime.now()}"
+            with open(case_dir / d / f, "w") as outfile:
+                outfile.write(file_contents)
+    return directories
+
+
+# def create_split_timestamps(case_dir: Path, timestamps: List[str]) -> None:
+#     for i in range(NUM_PROCESSORS):
+#         for timestamp in timestamps:
+#             (case_dir / f"processor{i}" / timestamp).mkdir()
+#             for var in TEST_VARIABLES:
+#                 (case_dir / f"processor{i}" / timestamp / var).touch()
+
+
+# def create_reconstructed_timestamps_without_done_marker(
+#     case_dir: Path, timestamps: List[str]
+# ) -> None:
+#     for timestamp in timestamps:
+#         (case_dir / timestamp).mkdir()
+#         for var in TEST_VARIABLES:
+#             (case_dir / timestamp / var).touch()
+
+
+# def create_reconstructed_timestamps_with_done_marker(
+#     case_dir: Path, timestamps: List[str]
+# ) -> None:
+#     create_reconstructed_timestamps_without_done_marker(case_dir, timestamps)
+#     for timestamp in timestamps:
+#         (case_dir / timestamp / RECONSTRUCTION_DONE_MARKER_FILENAME).touch()
+
+
+# def create_reconstructed_tars(case_dir: Path, timestamps: List[str]) -> None:
+#     for timestamp in timestamps:
+#         (case_dir / f"{timestamp}.tar").touch()
+
+
+# Test initialization
+
+
+def test_in_valid_case_dir(case_dir: Path) -> None:
+    # This should not raise an error
+    SlurmJobManager(
+        case_dir=case_dir,
+        job_sfile=JOB_SFILE_NAME,
+        job_id=SLURM_JOB_ID,
+        compress_sfile=COMPRESS_SFILE_NAME,
+    )
+
+
+@pytest.mark.parametrize(
+    "leave_out_file", [JOB_SFILE_NAME, COMPRESS_SFILE_NAME]
+)
+def test_in_bad_case_dir(case_dir: Path, leave_out_file: str) -> None:
+    # Remove the file to leave out
+    (case_dir / leave_out_file).unlink()
+    with pytest.raises(ValueError):
+        SlurmJobManager(
+            case_dir=case_dir,
+            job_sfile=JOB_SFILE_NAME,
+            job_id=SLURM_JOB_ID,
+            compress_sfile=COMPRESS_SFILE_NAME,
+        )
+
+
+def test_can_extract_job_name(cluster: SlurmJobManager) -> None:
+    assert cluster.job_name == "sample_job_name"
+
+
+# Mock Task so that it does not run anything
+
+
+def print_instead_of_running(task: Task, block: bool = False) -> None:
+    print(f"Command: {task.command}", end="")
+
+
+@pytest.fixture
+def mocked_task_run():
+    with mock.patch.object(Task, "run", autospec=True) as _mocked_task_run:
+        _mocked_task_run.side_effect = print_instead_of_running
+        yield _mocked_task_run
+
+
+@pytest.fixture
+def mocked_get_job_name():
+    with mock.patch.object(
+        SlurmJobManager, "get_job_name", autospec=True
+    ) as _mocked_get_job_name:
+        _mocked_get_job_name.return_value = "sample_compress_job_name"
+        yield _mocked_get_job_name
+
+
+@pytest.fixture
+def mocked_get_job_status_pending():
+    with mock.patch.object(
+        SlurmJobManager, "get_job_status", autospec=True
+    ) as _mocked_get_job_id:
+        _mocked_get_job_id.return_value = "PENDING"
+        yield _mocked_get_job_id
+
+
+@pytest.fixture
+def mocked_get_job_status_does_not_exist():
+    with mock.patch.object(
+        SlurmJobManager, "get_job_status", autospec=True
+    ) as _mocked_get_job_id:
+        _mocked_get_job_id.return_value = "JOB_NOT_FOUND"
+        yield _mocked_get_job_id
+
+
+def assert_task_did_not_run(capfd) -> None:
+    out, err = capfd.readouterr()
+    assert out == ""
+
+
+def assert_task_ran(capfd) -> None:
+    out, err = capfd.readouterr()
+    assert out.startswith("Command: ")
+
+
+# Test requeueing
+
+
+def test_requeue_generates_correct_command(
+    cluster: SlurmJobManager, mocked_task_run, capfd
+) -> None:
+    cluster.requeue_job()
+    out, err = capfd.readouterr()
+    cwd = os.getcwd()
+    assert (
+        out == f"Command: cd {cluster.case_dir}"
+        f" && sbatch --parsable -d afterany:{SLURM_JOB_ID} {JOB_SFILE_NAME}"
+        f" && cd {cwd}"
+    )
+
+
+def test_requeue_does_not_run_twice(
+    cluster: SlurmJobManager, mocked_task_run, capfd
+) -> None:
+    # Requeue the job and read the buffer
+    cluster.requeue_job()
+    _, _ = capfd.readouterr()
+    # Try to requeue again and read the buffer
+    cluster.requeue_job()
+    out, err = capfd.readouterr()
+    assert_task_did_not_run(capfd)
+
+
+def test_requeue_handles_missing_sfile(
+    cluster: SlurmJobManager, mocked_task_run, capfd
+) -> None:
+    # Remove the sfile
+    (cluster.case_dir / cluster.job_sfile).unlink()
+    with pytest.raises(FileNotFoundError):
+        cluster.requeue_job()
+    assert_task_did_not_run(capfd)
+
+
+def test_requeue_handles_changed_job_name(
+    cluster: SlurmJobManager, mocked_task_run, capfd
+) -> None:
+    # Instead of changing the job name in the sfile, change it in the
+    # SlurmJobManager instance so that it no longer matches the sfile to
+    # simulate this
+    cluster.job_name = "very_random_new_job_name"
+    with pytest.raises(AttributeError):
+        cluster.requeue_job()
+    assert_task_did_not_run(capfd)
+
+
+# Test compressing
+
+
+def test_compress_handles_empty_tgz_file_name(
+    cluster: SlurmJobManager, files_list: List[str], mocked_task_run, capfd
+) -> None:
+    with pytest.raises(ValueError):
+        cluster.compress(tgz_file="", files=files_list)
+    assert_task_did_not_run(capfd)
+
+
+def test_compress_handles_tgz_file_name_with_spaces(
+    cluster: SlurmJobManager, files_list: List[str], mocked_task_run, capfd
+) -> None:
+    with pytest.raises(ValueError):
+        cluster.compress(tgz_file="some tgz file.tgz", files=files_list)
+    assert_task_did_not_run(capfd)
+
+
+def test_compress_handles_empty_files_list(
+    cluster: SlurmJobManager, files_list: List[str], mocked_task_run, capfd
+) -> None:
+    with pytest.raises(ValueError):
+        cluster.compress(tgz_file="some_tgz_file.tgz", files=[])
+    assert_task_did_not_run(capfd)
+
+
+def test_compress_handles_files_with_spaces_in_name(
+    cluster: SlurmJobManager, mocked_task_run, capfd
+) -> None:
+    files_list = ["file1", "file_2", "file 3", "file_4", "file5"]
+    # Create these files
+    for f in files_list:
+        (cluster.case_dir / f).touch()
+    with pytest.raises(ValueError):
+        cluster.compress(tgz_file="some_tgz_file.tgz", files=files_list)
+    assert_task_did_not_run(capfd)
+
+
+def test_compress_handles_non_existent_files(
+    cluster: SlurmJobManager, files_list: List[str], mocked_task_run, capfd
+) -> None:
+    # Remove one of the files
+    (cluster.case_dir / files_list[2]).unlink()
+    with pytest.raises(FileNotFoundError):
+        cluster.compress(tgz_file="some_tgz_file.tgz", files=files_list)
+    assert_task_did_not_run(capfd)
+
+
+def test_compress_handles_missing_sfile(
+    cluster: SlurmJobManager, files_list: List[str], mocked_task_run, capfd
+) -> None:
+    # Remove the sfile
+    (cluster.case_dir / cluster.compress_sfile).unlink()
+    with pytest.raises(FileNotFoundError):
+        cluster.compress(tgz_file="some_tgz_file.tgz", files=files_list)
+    assert_task_did_not_run(capfd)
+
+
+def test_compress_generates_correct_command_for_files(
+    cluster: SlurmJobManager, files_list: List[str]
+) -> None:
+    tgz_file = "some_tgz_file.tgz"
+    compress_command = cluster._create_compress_command(
+        tgz_file=tgz_file, files=files_list
+    )
+    true_tar_command = "tar -czvf some_tgz_file.tgz.inprogress.$SLURM_JOB_ID 1e-4 0.001 0.01 0.1 1 10.0 100"
+    true_compress_command = (
+        "mv some_tgz_file.tgz.queued some_tgz_file.tgz.inprogress.$SLURM_JOB_ID"
+        + " && "
+        + true_tar_command
+        + " && mv some_tgz_file.tgz.inprogress.$SLURM_JOB_ID some_tgz_file.tgz"
+        + " && echo Done compressing some_tgz_file.tgz!"
+    )
+    assert compress_command == true_compress_command
+
+
+def test_compress_generates_correct_command_for_directories(
+    cluster: SlurmJobManager, directories_list: List[str]
+) -> None:
+    test_tgz = "some_tgz_file.tgz"
+    compress_command = cluster._create_compress_command(
+        tgz_file=test_tgz, files=directories_list
+    )
+    true_tar_command = f"tar -czvf {test_tgz}.inprogress.$SLURM_JOB_ID 1e-4 0.001 0.01 0.1 1 10.0 100"
+    true_compress_command = (
+        f"mv {test_tgz}.queued {test_tgz}.inprogress.$SLURM_JOB_ID"
+        + " && "
+        + true_tar_command
+        + f" && mv {test_tgz}.inprogress.$SLURM_JOB_ID {test_tgz}"
+        + f" && echo Done compressing {test_tgz}!"
+    )
+    assert compress_command == true_compress_command
+
+
+def test_compress_generates_correct_sfile(
+    cluster: SlurmJobManager, files_list: List[str]
+) -> None:
+    # Read the contents of the template sfile in
+    with open(cluster.case_dir / COMPRESS_SFILE_NAME, "r") as origfile:
+        sfile_contents = origfile.read()
+    # Create an arbitrary command that should be added in
+    random_command = "this is random && it needs to be added into the sfile"
+    # Add it to what the sfile_contents should become
+    sfile_contents += f"\n\n{random_command}"
+    # Now create the new sfile
+    with cluster._create_compress_sfile(
+        random_command
+    ) as generated_sfile_path:
+        # Read the contents of the generated file
+        with open(generated_sfile_path, "r") as genfile:
+            generated_contents = genfile.read()
+    assert generated_contents == sfile_contents
+
+
+def test_compress_deletes_generated_sfile_after_use(
+    cluster: SlurmJobManager, files_list: List[str]
+) -> None:
+    with cluster._create_compress_sfile(
+        "random command"
+    ) as generated_sfile_path:
+        # The generated sfile should exist here
+        assert generated_sfile_path.is_file()
+    # And it should be cleaned up by here
+    assert not generated_sfile_path.is_file()
+
+
+def test_compress_does_not_run_when_queued(
+    cluster: SlurmJobManager,
+    files_list: List[str],
+    mocked_task_run,
+    mocked_get_job_status_pending,
+    capfd,
+) -> None:
+    tgz_file = "test_tgz_file.tgz"
+    # Simulate that the compression task is queued by creating the queued file
+    # placeholder
+    queued_file_placeholder = tgz_file + ".queued"
+    (cluster.case_dir / queued_file_placeholder).touch()
+    cluster.compress(tgz_file=tgz_file, files=files_list)
+    assert_task_did_not_run(capfd)
+
+
+def test_compress_does_not_run_when_inprogress(
+    cluster: SlurmJobManager,
+    files_list: List[str],
+    mocked_task_run,
+    mocked_get_job_status_pending,
+    capfd,
+) -> None:
+    tgz_file = "test_tgz_file.tgz"
+    # Simulate that the compression task is in progress by creating the in
+    # progress file
+    inprogress_file = tgz_file + f".inprogress.{COMPRESS_JOB_ID}"
+    (cluster.case_dir / inprogress_file).touch()
+    cluster.compress(tgz_file=tgz_file, files=files_list)
+    assert_task_did_not_run(capfd)
+
+
+def test_compress_does_not_run_when_completed(
+    cluster: SlurmJobManager,
+    files_list: List[str],
+    mocked_task_run,
+    mocked_get_job_status_pending,
+    capfd,
+) -> None:
+    tgz_file = "test_tgz_file.tgz"
+    # Simulate that the compression task is complete by creating the tgz_file
+    (cluster.case_dir / tgz_file).touch()
+    cluster.compress(tgz_file=tgz_file, files=files_list)
+    assert_task_did_not_run(capfd)
+
+
+def test_compress_does_run_when_not_started(
+    cluster: SlurmJobManager,
+    files_list: List[str],
+    mocked_task_run,
+    mocked_get_job_status_pending,
+    capfd,
+) -> None:
+    tgz_file = "test_tgz_file.tgz"
+    cluster.compress(tgz_file=tgz_file, files=files_list)
+    true_command = (
+        f"Command: touch {cluster.case_dir}/{tgz_file}.queued"
+        + f" && sbatch {cluster.case_dir}/{COMPRESS_SFILE_NAME}.filled"
+    )
+    out, err = capfd.readouterr()
+    assert out == true_command
